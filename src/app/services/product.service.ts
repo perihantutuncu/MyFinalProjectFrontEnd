fd423cf824e5e4ae512f7b2bbbbab581ee28aa4a
perihantutuncu <perihantutuncu@gmail.com>
2021-03-27 17:59:24 +0300

    feat: product service added

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { ProductResponseModel } from '../models/productResposeModal';
+import { Observable } from 'rxjs';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class ProductService {
+
+	apiUrl = "https://localhost:44358/api/products/getall"
+
+	constructor(private httpClient: HttpClient) { }
+
+	getProducts():Observable<ProductResponseModel> {
+		return this.httpClient.get<ProductResponseModel>(this.apiUrl)
+	}
+}
