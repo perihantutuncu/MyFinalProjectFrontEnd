05fbfe21daca9a187bc93f9894d5f356fd55ac2f
perihantutuncu <perihantutuncu@gmail.com>
2021-03-27 20:50:34 +0300

    feat: get products by category added

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -3,24 +3,33 @@ import { Category } from 'src/app/models/category';
 import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
-  selector: 'app-category',
-  templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+	selector: 'app-category',
+	templateUrl: './category.component.html',
+	styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit {
 
-  categories: Category[] = []
-  dataLoaded = false
-  constructor(private categoryService:CategoryService) { }
+	categories: Category[]
+	currentCategory: Category
+	dataLoaded = false
+	constructor(private categoryService:CategoryService) { }
 
-  ngOnInit(): void {
-    this.getCategories()
-  }
+	ngOnInit(): void {
+	this.getCategories()
+	}
 
-  getCategories() {
-    this.categoryService.getCategories().subscribe(response => {
+	getCategories() {
+	this.categoryService.getCategories().subscribe(response => {
 			this.categories = response.data
 			this.dataLoaded = true
 		})
-  }
+	}
+
+	setCurrentCategory(category:Category) {
+		this.currentCategory = category
+	}
+
+	getCurrentCategoryClass(category:Category) {
+		return category == this.currentCategory ? "list-group-item active" : "list-group-item"
+	}
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
-import { ListResponseModel } from '../models/ListResponseModel';
+import { ListResponseModel } from '../models/listResponseModel';
 import { Product } from '../models/product';
 
 @Injectable({
@@ -9,11 +9,17 @@ import { Product } from '../models/product';
 })
 export class ProductService {
 
-	apiUrl = "https://localhost:44358/api/products/getall"
+	apiUrl = "https://localhost:44358/api/"
 
-	constructor(private httpClient: HttpClient) { }
+	constructor(private httpClient:HttpClient) { }
 
-	getProducts():Observable<ListResponseModel<Product>> {
-		return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl)
+	getProducts(): Observable<ListResponseModel<Product>> {
+		let newPath = this.apiUrl + "products/getall"
+		return this.httpClient.get<ListResponseModel<Product>>(newPath)
+	}
+
+	getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>> {
+		let newPath = this.apiUrl + "products/getbycategory?categoryId=" + categoryId
+		return this.httpClient.get<ListResponseModel<Product>>(newPath)
 	}
 }
